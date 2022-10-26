1eb4fb61b5d8bfdf2251a2b970f48b26f00d5d0c
Carlos S <camilo.suarez85@gmail.com>
2022-10-26 15:05:47 -0500

    adding filter

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var isFavoriteOnly = false
+    
+    var filteredItems: [Landmark] {
+        landmarks.filter { landamark in
+            (!isFavoriteOnly || landamark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetails(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
-                        
+            List {
+                Toggle(isOn: $isFavoriteOnly) {
+                    Text("Favorites")
                 }
+                ForEach(filteredItems) { landmark in
+                    NavigationLink {
+                        LandmarkDetails(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                        
+                    }
+                }
+                .navigationTitle("Places")
             }
-            .navigationTitle("Places")
         }
     }
 }
